9cefd2454a9061f2b2db93ad5c18e9790b8648df
Juan Sanchez <juan.sanchez@juanleonardosanchez.com>
2026-08-23 05:22:25 -0400

    fix: relax diagnostics mode assertion on Windows

--- internal/diagnostics/diagnostics_test.go
+++ internal/diagnostics/diagnostics_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"testing"
 
@@ -32,7 +33,7 @@ func TestWriteBundleIsPrivateAndBounded(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if info.Mode().Perm() != 0o600 {
+	if runtime.GOOS != "windows" && info.Mode().Perm() != 0o600 {
 		t.Fatalf("mode=%o", info.Mode().Perm())
 	}
 }
